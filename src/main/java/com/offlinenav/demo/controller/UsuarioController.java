c7920beefc55b1fd5751b609e30e1abce0d6342b
Ezequiel Bispo de Jesus <ezequiel.bispo@kyndryl.com>
2024-10-12 12:17:34 -0300

    Classe Controller corrigida.

--- src/main/java/com/offlinenav/demo/controller/UsuarioController.java
+++ src/main/java/com/offlinenav/demo/controller/UsuarioController.java
@@ -3,9 +3,13 @@ package com.offlinenav.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import com.offlinenav.demo.model.UsuarioModel;
+import com.offlinenav.demo.service.UsuarioService;
+import com.offlinenav.demo.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
@@ -16,7 +20,7 @@ public class UsuarioController {
     private UsuarioRepository UsuarioRepository;
 
     @GetMapping("/{id}")
-    public Usuario buscarId(@PathVariable int id) {
+    public UsuarioModel buscarId(@PathVariable int id) {
         return service.getUsuario(id);
     }
     
